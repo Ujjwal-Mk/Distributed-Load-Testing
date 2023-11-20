7561b98bf41b05c79619beafef2985ad672099ee
Ujjwal-Mk <ujjwalmk16@gmail.com>
2023-11-20 21:31:03 +0530

    web_server threading

--- web_server.py
+++ web_server.py
@@ -1,21 +1,49 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
+from prometheus_client import generate_latest, Counter
+import json
+
+# Global counters
+counter_requests = Counter('http_server_requests_total', 'Total number of requests received')
+counter_responses = Counter('http_server_responses_total', 'Total number of responses sent')
 
 class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # Respond to GET requests
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+        global counter_requests, counter_responses
 
-        # Send the response body
-        response_message = "Hello World!"
-        print(response_message)
-        self.wfile.write(response_message.encode('utf-8'))
+        if self.path == '/ping':
+            counter_requests.inc()
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+            response_message = '{"message": "ping"}'
+            print("HELLO WORLD")
+            self.wfile.write(response_message.encode('utf-8'))
+            counter_responses.inc()
+        elif self.path == '/metrics':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain; version=0.0.4')
+            self.end_headers()
+            
+            # Get the current values of the counters
+            requests_count = counter_requests._value.get()
+            responses_count = counter_responses._value.get()
+            
+            # Include counter values in the response message
+            response_message = f'{{"message": "metrics", "requests_count": {requests_count}, "responses_count": {responses_count}}}'
+            self.wfile.write(response_message.encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            response_message = 'Not Found'
+            self.wfile.write(response_message.encode('utf-8'))
 
 if __name__ == '__main__':
-    # Set the server address (empty string means localhost) and port
     server_address = ('', 9090)
-    httpd = HTTPServer(server_address, MyRequestHandler)
+    httpd = ThreadingHTTPServer(server_address, MyRequestHandler)
+
     print('Starting the server on port 9090...')
     try:
         httpd.serve_forever()
